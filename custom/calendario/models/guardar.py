7c39e4e5a2cba9f649b9dbcd84a1797a17046235
JoaquinWeb <joaquincallejon12@gmail.com>
2020-12-24 17:57:48 -0300

    se sube el modulo calendario

---
+++ custom/calendario/models/guardar.py
@@ -0,0 +1,11 @@
+from odoo import models, fields, api
+
+
+class Calendario(models.Model):
+    _name = 'calendario.guardar'
+
+    date = fields.Date(string="fecha", required=True )
+    name = fields.Char(string="Nombre", required=True)
+    hour = fields.Date(string="hora", required=True)
+    description = fields.Text(string="descripcion", required=True)
+    n_chacis = fields.Char(string="numero de chasis", required=True)
