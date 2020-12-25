9620fb649d8e05cc6ac4a5353747c1531e5199ea
JoaquinWeb <joaquincallejon12@gmail.com>
2020-12-25 16:52:08 -0300

    Actualizacion de calendario

--- custom/calendario/models/guardar.py
+++ custom/calendario/models/guardar.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 class Calendario(models.Model):
     _name = 'calendario.guardar'
 
-    date = fields.DateTime(string="fecha", required=True )
+    date = fields.Date(string="fecha", required=True )
     name = fields.Char(string="Nombre", required=True)
     description = fields.Text(string="descripcion", required=True)
     n_chacis = fields.Char(string="numero de chasis", required=True)
