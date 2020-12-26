d339a3ac656f0089ecb97b0618455ef153963b4e
JoaquinWeb <joaquincallejon12@gmail.com>
2020-12-25 22:49:42 -0300

    calendario actualizacion atributos de calendario

--- custom/calendario/models/guardar.py
+++ custom/calendario/models/guardar.py
@@ -7,4 +7,4 @@ class Calendario(models.Model):
     date = fields.Date(string="fecha", required=True )
     name = fields.Char(string="Nombre", required=True)
     description = fields.Text(string="descripcion", required=True)
-    n_chacis = fields.Char(string="numero de chasis", required=True)
+    n_chacis = fields.Text(string="numero de chasis", required=True)
